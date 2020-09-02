1cb391775405f26c222fd4442ce4a39bb36bf19b
Fabimauri47 <44916470+Fabimauri47@users.noreply.github.com>
2020-09-02 14:55:05 -0500

    especificacion metodos

---
+++ Hangman/src/main/java/hangman/model/BonusScore.java
@@ -0,0 +1,6 @@
+public class BonusScore implements hangman.model.dictionary.GameScore {
+    @Override
+    public int calculateScore(int correctCount, int incorrectCount) {
+        return 0;
+    }
+}
---
+++ Hangman/src/main/java/hangman/model/OrignalScore.java
@@ -0,0 +1,7 @@
+public class OrignalScore implements hangman.model.dictionary.GameScore {
+
+    @Override
+    public int calculateScore(int correctCount, int incorrectCount) {
+        return 0;
+    }
+}
